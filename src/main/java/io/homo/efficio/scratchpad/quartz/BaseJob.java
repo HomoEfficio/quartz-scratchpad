6e01d6d54042320386502bd9703b4dab3e70dc76
Homo Efficio <homo.efficio@gmail.com>
2018-08-12 01:22:13 +0900

    Applied Template Method pattern

--- src/main/java/io/homo/efficio/scratchpad/quartz/BaseJob.java
+++ src/main/java/io/homo/efficio/scratchpad/quartz/BaseJob.java
@@ -1,5 +1,6 @@
 package io.homo.efficio.scratchpad.quartz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,12 +9,28 @@ import org.quartz.JobExecutionException;
  * @author homo.efficio@gmail.com
  * created on 2018-08-12
  */
+@Slf4j
 public abstract class BaseJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        beforeExecute(context);
         doExecute(context);
+        afterExecute(context);
+        scheduleNextJob(context);
+    }
+
+    private void beforeExecute(JobExecutionContext context) {
+        log.info("%%% Before executing job");
     }
 
     protected abstract void doExecute(JobExecutionContext context);
+
+    private void afterExecute(JobExecutionContext context) {
+        log.info("%%% After executing job");
+    }
+
+    private void scheduleNextJob(JobExecutionContext context) {
+        log.info("$$$ Schedule Next Job");
+    }
 }
