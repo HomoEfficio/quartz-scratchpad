0c511d6e4c5586c4de4c8511006288d90c15c099
Homo Efficio <homo.efficio@gmail.com>
2018-08-12 00:41:50 +0900

    Added trigger

--- src/test/java/io/homo/efficio/scratchpad/quartz/QuartzTest.java
+++ src/test/java/io/homo/efficio/scratchpad/quartz/QuartzTest.java
@@ -2,8 +2,12 @@ package io.homo.efficio.scratchpad.quartz;
 
 import org.junit.Test;
 import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
 
 import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
  * @author homo.efficio@gmail.com
@@ -14,6 +18,9 @@ public class QuartzTest {
     @Test
     public void helloJob() {
         JobDetail jobDetail = newJob(HelloJob.class)
+                .build();
+
+        Trigger trigger = newTrigger()
                 .build();
     }
 }
