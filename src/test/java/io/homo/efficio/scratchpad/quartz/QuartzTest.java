4ba9c6413080caa6f8fae0d63598da447956871d
Homo Efficio <homo.efficio@gmail.com>
2018-08-12 00:47:57 +0900

    Added scheduler and execution success

--- src/test/java/io/homo/efficio/scratchpad/quartz/QuartzTest.java
+++ src/test/java/io/homo/efficio/scratchpad/quartz/QuartzTest.java
@@ -3,6 +3,7 @@ package io.homo.efficio.scratchpad.quartz;
 import org.junit.Test;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -16,11 +17,23 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class QuartzTest {
 
     @Test
-    public void helloJob() {
+    public void helloJob() throws SchedulerException, InterruptedException {
+
+        // Job 구현 내용이 담긴 HelloJob으로 JobDetail 생성
         JobDetail jobDetail = newJob(HelloJob.class)
                 .build();
 
+        // 실행 시점을 결정하는 Trigger 생성
         Trigger trigger = newTrigger()
                 .build();
+
+        // 스케줄러 실행 및 JobDetail과 Trigger 정보로 스케줄링
+        Scheduler defaultScheduler = StdSchedulerFactory.getDefaultScheduler();
+        defaultScheduler.start();
+        defaultScheduler.scheduleJob(jobDetail, trigger);
+        Thread.sleep(3 * 1000);  // Job이 실행될 수 있는 시간 여유를 준다
+
+        // 스케줄러 종료
+        defaultScheduler.shutdown(true);
     }
 }
